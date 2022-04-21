08b7e17513afdb551d06b5cbdc4364ae9c95ec49
KKcuber <kkcubes@gmail.com>
2022-04-22 04:34:49 +0530

    added ngo dashboard

--- petlove/lib/screens/NGO_home_page.dart
+++ petlove/lib/screens/NGO_home_page.dart
@@ -13,6 +13,7 @@ import 'package:petlove/screens/account_page.dart';
 import 'package:petlove/mainkk.dart';
 import 'package:petlove/screens/register_NGO.dart';
 import 'package:petlove/screens/join_NGO.dart';
+import 'package:petlove/screens/ngo_request_display.dart';
 
 class NGOHomePage extends StatefulWidget {
   const NGOHomePage({Key? key, required String? uid})
@@ -49,18 +50,18 @@ class _NGOHomePageState extends State<NGOHomePage> {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Color.fromARGB(255, 4, 50, 88),
-          /**leading: Container(
-          color: Color.fromARGB(255, 57, 152, 216),
-          padding: EdgeInsets.all(3),
-          /** PetCare Logo **/
-            child: Flexible(
-            flex: 1,
-            child: Image.asset(
-              'assets/petcare_image.PNG',
-              height: 50,
-            ),
-          ),
-        ),**/ //Container
+        //   leading: Container(
+        //   color: Color.fromARGB(255, 57, 152, 216),
+        //   padding: EdgeInsets.all(3),
+        //   /** PetCare Logo **/
+        //     child: Flexible(
+        //     flex: 1,
+        //     child: Image.asset(
+        //       'assets/petcare_image.PNG',
+        //       height: 50,
+        //     ),
+        //   ),
+        // ), //Container
           title: Text('Welcome to petcare app'),
           centerTitle: true,
         ),
@@ -109,6 +110,19 @@ class _NGOHomePageState extends State<NGOHomePage> {
                 //             user: _user,
                 //           )),
                 // );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.request_quote),
+              title: Text("Help Requests", style: TextStyle(fontSize: 18)),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => NGORequestsDisplay(
+                            uid: _uid,
+                          )),
+                );
               },
             ),
           ]),
